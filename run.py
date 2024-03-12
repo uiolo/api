62b96bae17bec5a7d53b791c9d9a1405ff195512
Vladislav <you@example.com>
2024-03-12 15:42:08 +0300

    i also add a comment number 2

--- run.py
+++ run.py
@@ -22,7 +22,7 @@ async def parse_info():
         async with session.get(os.environ.get('url', 'https://ya.ru')) as response:
             return f'Code: {response.status}'
 # 1
-# 2
+# NOW IT'S MINE COMMENT! AHAHAHHAHAHA AJAJAJAJ! khe khe
 # 3
 
 @app.route('/drinks/', methods=['DELETE'])
